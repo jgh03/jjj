0fe88653b19c3fa4f7ef30b5e8ee9325f7a4f52b
jgh03 <jgabrielle@student.ciputra.ac.id>
2021-12-06 01:16:21 +0700

    Initial check-in of module jjjj

---
+++ jjjj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jjjj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
